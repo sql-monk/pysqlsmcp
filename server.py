553136bb16e690e1d2b94c45f4fec11a7f7a3ef7
sql-monk <sql-monk@users.noreply.github.com>
2026-03-31 11:51:29 +0300

    Refactor query registration in server.py to enhance modularity and maintainability

--- server.py
+++ server.py
@@ -1,16 +1,18 @@
 import argparse
 import uvicorn
 from mcp.server.fastmcp import FastMCP
-from tools.select_query import register as reg1
-from tools.get_database_permission import register as reg2
-from tools.get_all_database_permission import register as reg3
-from tools.required_additional_permission import register as reg4
+from tools.execute_query import register as reg1
+from tools.explain_query import register as reg2
+from tools.get_database_permission import register as reg3
+from tools.get_all_database_permission import register as reg4
+from tools.required_additional_permission import register as reg5
 
 mcp = FastMCP("pysqlsmcp")
 reg1(mcp)
 reg2(mcp)
 reg3(mcp)
 reg4(mcp)
+reg5(mcp)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
